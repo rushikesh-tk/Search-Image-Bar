bd9d186e4f43dc01a256fc69f419665bf9e44cb9
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2020-07-03 12:13:02 +0530

    added ImageList Component

---
+++ src/components/ImageList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ImageCard from './ImageCard';
+import './ImageList.css';
+
+const ImageList = (props) => {
+
+	const image = props.images.map((image) => {
+		return <ImageCard key={image.id} image={image}/>
+		})
+
+	return (
+		<div className="image-list pa3">{image}</div>
+	);
+}
+
+export default ImageList;
