c3a5e97c301b5cf388caaec72a2815c367e62517
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2020-07-03 12:16:36 +0530

    added ImageCard Component

---
+++ src/components/ImageCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+
+	constructor(props){
+		super(props);
+
+		this.state = { span : 0 };
+
+		this.imageRef = React.createRef();
+	}
+
+	componentDidMount(){
+		this.imageRef.current.addEventListener('load', this.setSpans);
+	}
+
+	setSpans = () => {
+		const height = this.imageRef.current.clientHeight;
+
+		const span = Math.ceil(height / 10);
+
+		this.setState({ span })
+	}
+
+	render(){
+		const { description, urls } = this.props.image;
+		return (
+			<div style={{ gridRowEnd : `span ${this.state.span}`}}>
+				<img className="grow shadow-1" ref={this.imageRef} alt={description} src={urls.regular}/>
+			</div>
+		);
+	}
+}
+
+export default ImageCard;
